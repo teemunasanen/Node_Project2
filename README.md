b02c8400f2f7ade2406de5044152cc12ad4bb221
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-03-19 22:30:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Node.js Project 2: Small Project on Templates
+
+Laurea University of Applied Sciences
+Teemu Näsänen 2021
---
+++ server.js
@@ -0,0 +1,26 @@
+const PORT = process.env.PORT || 8081;
+
+const express = require("express");
+const app = express();
+
+
+app.use(express.static("./public/"));
+
+app.locals.pretty = true;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+
+
+app.get("*", function (req, res) {
+    res.send('Cant find the requested page', 404);
+});
+
+
+app.listen(PORT);
+console.log(PORT + " is the magic port");
