cafda26dd0f79a2f40dc026959fbe6ef0b2abac4
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-03-19 22:40:42 +0200

    prettier

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const PORT = process.env.PORT || 8081;
 const express = require("express");
 const app = express();
 
-
 app.use(express.static("./public/"));
 
 app.locals.pretty = true;
@@ -15,12 +14,9 @@ app.get("/", function (req, res) {
   res.render("pages/index");
 });
 
-
-
 app.get("*", function (req, res) {
-    res.send('Cant find the requested page', 404);
+  res.send("Cant find the requested page", 404);
 });
-
 
 app.listen(PORT);
 console.log(PORT + " is the magic port");
